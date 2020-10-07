7e33ff3b4210bcb7155a02d03083855890ee5e00
unknown <titus.brasov@gmail.com>
2020-10-07 21:43:54 +0300

    Sedinta 07102020

---
+++ 07102020intructiuni.py
@@ -0,0 +1,26 @@
+#if conditie1:
+#    sintaxa 1
+#elif conditie2
+#    sintaxa 2
+#else:
+#    sintaxa 3
+
+a = 1
+b = 2
+c = 3
+
+#if a > 4:
+#    print(a)
+#elif a > b:
+#    print(b)
+#elif c < a:
+#    print (c)
+# else:
+#    print("nici un rezultat gasit")
+
+d = None
+
+c = "sss"
+if a == 1:
+    c = "ss"
+print(c)
